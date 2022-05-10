1b84b3916f7f29ca7429a16a00ac0db317dfa24f
nerd00f <alancsystem@gmail.com>
2022-05-10 19:16:10 -0300

    aula 2

---
+++ 0. linux/scripts.sh
@@ -0,0 +1,23 @@
+# mostra informações do sistema
+uname -a
+# mostra informações do usuario
+whoami
+# troca de usuario
+su - usuario
+# executar comando como super usuario
+sudo comando
+# mostra diretorio atual
+pwd
+# lista conteudo do diretorio
+ls
+# lista informações de arquivos
+ls -l
+# lista diretorios ocultos
+ls -a
+# listar arquivos ou diretorios especificos
+ls Desktop/*.pdf
+# buscar comando ja digitado
+# * atalho de teclado *
+CTRL + R
+# alterar diretorio
+cd arquivo
