275d0499d5255292a5f6b69a7edd55ea417bbf39
nerd00f <alancsystem@gmail.com>
2022-05-22 16:02:09 -0300

    aula 3

--- 0. linux/scripts.sh
+++ 0. linux/scripts.sh
@@ -10,5 +10,19 @@ ls -l            #| lista informações de arquivos
 ls -a            #| lista diretorios ocultos
 ls Desktop/*.pdf #| listar arquivos ou diretorios especificos
 CTRL + R         #| *atalho de teclado* buscar comando ja digitado 
-cd arquivo       #| alterar diretorio
+cd diretorio     #| alterar diretorio
 
+# - - aula 3
+
+file arquivo            #| mostra informações sobre um arquivo
+cat arquivo             #| mostra o que esta escrito dentro do arquivo
+touch arquivo           #| cria um arquivo
+rm arquivo              #| remove um arquivo
+nano arquivo            #| editar um arquivo usando o nano
+mkdir diretorio         #| cria um diretorio
+rmdir diretorio         #| remove um diretorio vazio
+mv origem destino       #| mover diretorios ou arquivos
+cp arquivo destino      #| copiar arquivo
+cp -r diretorio destino #| copiar diretorio
+mv arquivo arquivo_novo #| renomear arquivo
+rm -rf diretorio        #| remove um diretorio que tem arquivos
